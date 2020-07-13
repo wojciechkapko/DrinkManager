5cd775bad1c97865b4399eb9e2d43f1056af9057
Wojciech Kapko <wojciech.kapko@gmail.com>
2020-07-13 18:23:05 +0200

    Remove helloworld from the Main class

--- CourseManagerConsole/Program.cs
+++ CourseManagerConsole/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace CourseManagerConsole
+﻿namespace CourseManagerConsole
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
