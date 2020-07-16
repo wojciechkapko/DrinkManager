a0ee018f4fed7cd8938d1696a31a803b7f617263
Wojciech Kapko <wojciech.kapko@gmail.com>
2020-07-16 23:14:05 +0200

    Remove test code from the program class

--- DrinkManagerConsole/Program.cs
+++ DrinkManagerConsole/Program.cs
@@ -8,21 +8,6 @@ namespace DrinkManagerConsole
     {
         private static void Main(string[] args)
         {
-            var drinks = new List<Drink>();
-
-            var creator = new DrinkCreator(drinks);
-            creator.AddNewDrink();
-
-            foreach (var drink in drinks)
-            {
-                Console.WriteLine(drink.Name);
-                Console.WriteLine(drink.Instructions);
-
-                foreach (var drinkIngredient in drink.Ingredients)
-                {
-                    Console.WriteLine($"{drinkIngredient.Name} {drinkIngredient.Amount}");
-                }
-            }
         }
     }
 }
