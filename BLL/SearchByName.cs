06a5433a8f2bdb233dc575fd3cf0d6733bd09e96
Marcin Siemiński <marcin.sieminski@outlook.com>
2020-07-17 07:41:30 +0200

    Initial commit

---
+++ BLL/SearchByName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class SearchByName
+    {
+        public List<Drink> SearchDrinkByName(string name)
+        {
+            List<Drink> drinksFound = null;
+
+            return drinksFound;
+        }
+    }
+}
