4d27ebf2f6ce65d0c8cde54685acd4043fe7f094
Marcin Siemiński <marcin.sieminski@outlook.com>
2020-07-19 22:06:22 +0200

    Add logic for loading drinks list, searching and writting results to console

--- DrinkManagerConsole/SearchDrink.cs
+++ DrinkManagerConsole/SearchDrink.cs
@@ -7,9 +7,13 @@ namespace DrinkManagerConsole
     {
         internal static void SearchDrinkByName()
         {
+            Console.Clear();
             Console.WriteLine("\nSearch drink by name\n");
-            var textToSearch = Utility.GetGenericData("Enter a drink name to find: ");
-
+            Console.Write("Enter a drink name to find: ");
+            var textToSearch = Console.ReadLine();
+            // load drinks list from file
+            //search for text in names
+            //return result to the console
         }
     }
 }
