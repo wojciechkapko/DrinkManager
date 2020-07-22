513654249b2fa7fb941403601ffcbcdecb21cd03
Wojciech Kapko <wojciech.kapko@gmail.com>
2020-07-22 17:30:36 +0200

    Change DrinkLoader - seperated the initial load and current drinks list extension option into two methods.

--- DrinkManagerConsole/Program.cs
+++ DrinkManagerConsole/Program.cs
@@ -8,9 +8,10 @@ namespace DrinkManagerConsole
         private static void Main()
         {
             // initial loading drinks list from file
-            var drinksListGlobal = DrinkLoader.AddDrinksFromFile();
+            var loader = new DrinkLoader();
+            var drinksListGlobal = loader.InitializeDrinksFromFile();
 
-            // temporary app logic - to be changed when all features are ready 
+            // temporary app logic - to be changed when all features are ready
             SearchDrinkByNameConsole.StartSearch(drinksListGlobal);
 
             Console.WriteLine("Press any key.");
