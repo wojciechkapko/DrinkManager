64bfb3b9c5b457993694e8f14caa0615d3dc88ec
Marcin Siemiński <marcin.sieminski@outlook.com>
2020-07-26 19:56:18 +0200

    Deleted temporary app logic

--- DrinkManagerConsole/Program.cs
+++ DrinkManagerConsole/Program.cs
@@ -12,31 +12,6 @@ namespace DrinkManagerConsole
             var loader = new DrinkLoader();
             var drinksListGlobal = loader.InitializeDrinksFromFile();
 
-            // temporary app logic - to be changed when all features are ready
-            bool runApp = true;
-            do
-            {
-                Console.Clear();
-                Console.Write("Search by name or ingredient or EXIT ? (n/i/e) ");
-                switch (Console.ReadKey().KeyChar)
-                {
-                    case 'n':
-                        SearchDrinkConsoleUi.StartSearch(drinksListGlobal, SearchCriterion.Name);
-                        break;
-                    case 'i':
-                        SearchDrinkConsoleUi.StartSearch(drinksListGlobal, SearchCriterion.Ingredients);
-                        break;
-                    case 'e':
-                        runApp = false;
-                        break;
-                    default:
-                        Console.WriteLine("\nI don't know what you mean - try again :)");
-                        break;
-                }
-            } while (runApp);
-
-            Console.WriteLine("\nPress any key to close.");
-            Console.ReadKey();
         }
     }
 }
