a7ec8920645819598aa45a8301adfbd46015151c
Wojciech Kapko <wojciech.kapko@gmail.com>
2020-07-27 01:23:54 +0200

    Add drink list extension from file option to the menu

--- DrinkManagerConsole/SearchDrinkConsoleUi.cs
+++ DrinkManagerConsole/SearchDrinkConsoleUi.cs
@@ -24,6 +24,7 @@ namespace DrinkManagerConsole
                         Console.Write($"\nEnter a drink {searchCriterion.ToString().ToLower()} to find: ");
                         drinksFound = SearchDrink.SearchByName(Console.ReadLine(), drinksList);
                         break;
+
                     case SearchCriterion.Ingredients:
                         Console.WriteLine("\nInstructions: \nYou can provide ONE or MORE ingredients - separated with a space. \nYou can search drinks containing ALL or ANY of provided ingredients.");
                         Console.Write("\nWould you like to display drinks containing: \n1. ALL provided ingredients \n2. ANY of provided ingredients\n(1/2) ");
@@ -33,12 +34,14 @@ namespace DrinkManagerConsole
                             case '1':
                                 searchOption = SearchDrinkOption.All;
                                 break;
+
                             case '2':
                                 searchOption = SearchDrinkOption.Any;
                                 break;
+
                             default:
                                 Console.WriteLine("\nI don't know what you mean - try again :)");
-                                searchOption = SearchDrinkOption.Any; //default initialization of local variable - default choice in case user fails to choose 
+                                searchOption = SearchDrinkOption.Any; //default initialization of local variable - default choice in case user fails to choose
                                 break;
                         }
                         Console.Write($"\n\nEnter a drink {searchCriterion.ToString().ToLower()} to find: ");
@@ -78,6 +81,30 @@ namespace DrinkManagerConsole
                     continueSearch = false;
                 }
             } while (continueSearch);
+        }
+
+        public static void AddMoreDrinksFromFile(List<Drink> drinksList)
+        {
+            Console.WriteLine("Please provide full path to the source file");
+            var path = Console.ReadLine();
+            var loader = new DrinkLoader();
+            try
+            {
+                loader.AddDrinksFromFile(drinksList, path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\n{e.Message}");
+                // This should be replaced with a method maybe like "WaitForAnyKey(string message)"
+                Console.WriteLine("\nPress any key to go back to the main menu.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("New drinks added!");
+            // This should be replaced with a method maybe like "WaitForAnyKey(string message)"
+            Console.WriteLine("\nPress any key to go back to the main menu.");
+            Console.ReadKey();
         }
     }
 }
