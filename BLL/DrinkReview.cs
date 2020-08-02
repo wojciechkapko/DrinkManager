783d7b8796a6ef7b3db1f6c618ba3c2f6e23a971
Joanna Bukowska <joabuk2@gmail.com>
2020-08-02 20:33:54 +0200

    New class DrinkReview added

---
+++ BLL/DrinkReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    public class DrinkReview
+    {
+        public int ReviewScore { get; set; }
+
+        public string ReviewText { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+    }
+}
