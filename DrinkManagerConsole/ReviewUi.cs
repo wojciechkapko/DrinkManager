5255dfead833eca6fbbc918881cf302d5e0299e8
Marcin Siemiński <marcin.sieminski@outlook.com>
2020-08-05 18:45:01 +0200

    Add ReviewUi class and EditReview method

---
+++ DrinkManagerConsole/ReviewUi.cs
@@ -0,0 +1,28 @@
+﻿using BLL;
+using System;
+
+namespace DrinkManagerConsole
+{
+    public class ReviewUi
+    {
+        public static void EditReview(Drink drink)
+        {
+            
+            Console.Clear();
+            SearchDrinkConsoleUi.WriteDrinkInfo(drink);
+
+            Console.WriteLine("Please provide new score:");
+            var newScore = Console.Read();
+            Console.WriteLine("Please write new review:");
+            Console.WriteLine(
+                "------------------------------------------------------------------------------------------------------------------");
+            var newReview = Console.ReadLine();
+            drink.DrinkReview.ReviewScore = newScore;
+            drink.DrinkReview.ReviewText = newReview;
+            drink.DrinkReview.ReviewDate = DateTime.Now;
+
+            Console.WriteLine("\nPress any key to go back.");
+            Console.ReadKey();
+        }
+    }
+}
