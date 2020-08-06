d6944eaa83c7b33b446dc66b33bc2026749992f9
Marcin Siemiński <marcin.sieminski@outlook.com>
2020-08-06 10:04:46 +0200

    Add constants MinScore and MaxScore to DrinkReview class

--- BLL/DrinkReview.cs
+++ BLL/DrinkReview.cs
@@ -4,6 +4,9 @@ namespace BLL
 {
     public class DrinkReview
     {
+        public const int MinScore = 0;
+        public const int MaxScore = 5;
+
         public int ReviewScore { get; set; }
 
         public string ReviewText { get; set; }
