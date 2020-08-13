5e2ba2c0f603997a7180db1e048f72b47c5f1915
Wojciech Kapko <wojciech.kapko@gmail.com>
2020-08-13 12:54:20 +0200

    change drink class and  loader method to avoid loading null ingredients from the source file

--- BLL/Drink.cs
+++ BLL/Drink.cs
@@ -1,120 +1,16 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace BLL
 {
     public class Drink
     {
-        private List<Ingredient> _ingredients;
-
-        [JsonProperty("idDrink")]
         public string Id { get; set; }
-
-        [JsonProperty("strDrink")]
         public string Name { get; set; }
-
-        [JsonProperty("strCategory")]
         public string Category { get; set; }
-
-        [JsonProperty("strAlcoholic")]
         public string AlcoholicInfo { get; set; }
-
-        [JsonProperty("strGlass")]
         public string GlassType { get; set; }
-
-        [JsonProperty("strInstructions")]
         public string Instructions { get; set; }
-
-        [JsonProperty("strIngredient1")]
-        public string IngredientName1 { get; set; }
-
-        [JsonProperty("strIngredient2")]
-        public string IngredientName2 { get; set; }
-
-        [JsonProperty("strIngredient3")]
-        public string IngredientName3 { get; set; }
-
-        [JsonProperty("strIngredient4")]
-        public string IngredientName4 { get; set; }
-
-        [JsonProperty("strIngredient5")]
-        public string IngredientName5 { get; set; }
-
-        [JsonProperty("strIngredient6")]
-        public string IngredientName6 { get; set; }
-
-        [JsonProperty("strIngredient7")]
-        public string IngredientName7 { get; set; }
-
-        [JsonProperty("strIngredient8")]
-        public string IngredientName8 { get; set; }
-
-        [JsonProperty("strIngredient9")]
-        public string IngredientName9 { get; set; }
-
-        [JsonProperty("strIngredient10")]
-        public string IngredientName10 { get; set; }
-
-        [JsonProperty("strIngredient11")]
-        public string IngredientName11 { get; set; }
-
-        [JsonProperty("strIngredient12")]
-        public string IngredientName12 { get; set; }
-
-        [JsonProperty("strIngredient13")]
-        public string IngredientName13 { get; set; }
-
-        [JsonProperty("strIngredient14")]
-        public string IngredientName14 { get; set; }
-
-        [JsonProperty("strIngredient15")]
-        public string IngredientName15 { get; set; }
-
-        [JsonProperty("strMeasure1")]
-        public string IngredientMeasure1 { get; set; }
-
-        [JsonProperty("strMeasure2")]
-        public string IngredientMeasure2 { get; set; }
-
-        [JsonProperty("strMeasure3")]
-        public string IngredientMeasure3 { get; set; }
-
-        [JsonProperty("strMeasure4")]
-        public string IngredientMeasure4 { get; set; }
-
-        [JsonProperty("strMeasure5")]
-        public string IngredientMeasure5 { get; set; }
-
-        [JsonProperty("strMeasure6")]
-        public string IngredientMeasure6 { get; set; }
-
-        [JsonProperty("strMeasure7")]
-        public string IngredientMeasure7 { get; set; }
-
-        [JsonProperty("strMeasure8")]
-        public string IngredientMeasure8 { get; set; }
-
-        [JsonProperty("strMeasure9")]
-        public string IngredientMeasure9 { get; set; }
-
-        [JsonProperty("strMeasure10")]
-        public string IngredientMeasure10 { get; set; }
-
-        [JsonProperty("strMeasure11")]
-        public string IngredientMeasure11 { get; set; }
-
-        [JsonProperty("strMeasure12")]
-        public string IngredientMeasure12 { get; set; }
-
-        [JsonProperty("strMeasure13")]
-        public string IngredientMeasure13 { get; set; }
-
-        [JsonProperty("strMeasure14")]
-        public string IngredientMeasure14 { get; set; }
-
-        [JsonProperty("strMeasure15")]
-        public string IngredientMeasure15 { get; set; }
-
+        public List<Ingredient> Ingredients { get; set; }
         public DrinkReview DrinkReview { get; set; }
 
         public bool isReviewed
@@ -127,99 +23,6 @@ namespace BLL
                 }
                 return false;
             }
-        }
-
-        public List<Ingredient> Ingredients
-        {
-            get
-            {
-                if (_ingredients == null)
-                {
-                    return new List<Ingredient>
-                    {
-                        new Ingredient
-                        {
-                            Name = IngredientName1,
-                            Amount = IngredientMeasure1
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName2,
-                            Amount = IngredientMeasure2
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName3,
-                            Amount = IngredientMeasure3
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName4,
-                            Amount = IngredientMeasure4
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName5,
-                            Amount = IngredientMeasure5
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName6,
-                            Amount = IngredientMeasure6
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName7,
-                            Amount = IngredientMeasure7
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName8,
-                            Amount = IngredientMeasure8
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName9,
-                            Amount = IngredientMeasure9
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName10,
-                            Amount = IngredientMeasure10
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName11,
-                            Amount = IngredientMeasure11
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName12,
-                            Amount = IngredientMeasure12
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName13,
-                            Amount = IngredientMeasure13
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName14,
-                            Amount = IngredientMeasure14
-                        },
-                        new Ingredient
-                        {
-                            Name = IngredientName15,
-                            Amount = IngredientMeasure15
-                        }
-                    };
-                }
-                else
-                {
-                    return _ingredients;
-                }
-            }
-            set => _ingredients = value;
         }
     }
 }
--- BLL/DrinkLoader.cs
+++ BLL/DrinkLoader.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace BLL
 {
@@ -26,12 +28,108 @@ namespace BLL
 
         private List<Drink> LoadFromFile(string path)
         {
-            List<Drink> newDrinks;
+            var newDrinks = new List<Drink>();
             try
             {
                 var drinksString = File.ReadAllText(path);
                 var jo = JObject.Parse(drinksString);
-                newDrinks = jo.SelectToken("drinks", false).ToObject<List<Drink>>();
+                dynamic deserializedJson = JsonConvert.DeserializeObject(jo.SelectToken("drinks", false).ToString());
+
+                foreach (var drink in deserializedJson)
+                {
+                    newDrinks.Add(new Drink
+                    {
+                        Id = drink.idDrink,
+                        Name = drink.strDrink,
+                        AlcoholicInfo = drink.strAlcoholic,
+                        Category = drink.strCategory,
+                        GlassType = drink.strGlass,
+                        Instructions = drink.strInstructions,
+                        Ingredients = new List<Ingredient>
+                        {
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient1,
+                                Amount = drink.strMeasure1
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient2,
+                                Amount = drink.strMeasure2
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient3,
+                                Amount = drink.strMeasure3
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient4,
+                                Amount = drink.strMeasure4
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient5,
+                                Amount = drink.strMeasure5
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient6,
+                                Amount = drink.strMeasure6
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient7,
+                                Amount = drink.strMeasure7
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient8,
+                                Amount = drink.strMeasure8
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient9,
+                                Amount = drink.strMeasure9
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient10,
+                                Amount = drink.strMeasure10
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient11,
+                                Amount = drink.strMeasure11
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient12,
+                                Amount = drink.strMeasure12
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient13,
+                                Amount = drink.strMeasure13
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient14,
+                                Amount = drink.strMeasure14
+                            },
+                            new Ingredient
+                            {
+                                Name = drink.strIngredient15,
+                                Amount = drink.strMeasure15
+                            }
+                        }
+                    });
+                }
+
+                foreach (var drink in newDrinks)
+                {
+                    drink.Ingredients = drink.Ingredients.Where(x => x.Name != null).ToList();
+                }
             }
             catch (Exception)
             {
