e8227e857be07d55d02a755ffc20d308653b1293
Wojciech Kapko <wojciech.kapko@gmail.com>
2020-08-31 01:27:10 +0200

    Add DrinksController, Add Drinks view, display all drinks in the drinks view

---
+++ DrinkManagerWeb/Controllers/DrinksController.cs
@@ -0,0 +1,27 @@
+﻿using DrinkManagerWeb.Data;
+using DrinkManagerWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DrinkManagerWeb.Controllers
+{
+    public class DrinksController : Controller
+    {
+        private readonly DrinkAppContext _db;
+
+        public DrinksController(DrinkAppContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new DrinksViewModel
+            {
+                Drinks = _db.Drinks.ToList()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ DrinkManagerWeb/Models/ViewModels/DrinksViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BLL;
+using System.Collections.Generic;
+
+namespace DrinkManagerWeb.Models.ViewModels
+{
+    public class DrinksViewModel
+    {
+        public List<Drink> Drinks { get; set; }
+    }
+}
