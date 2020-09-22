5e37d2eb6ccc2fa5880d0a7f83ebe4797f2d8638
wkapko <wojciech.kapko@gmail.com>
2020-09-22 22:36:54 +0200

    Add create view and logic

--- DrinkManagerWeb/Controllers/DrinksController.cs
+++ DrinkManagerWeb/Controllers/DrinksController.cs
@@ -1,10 +1,11 @@
 ﻿using BLL;
 using DrinkManagerWeb.Data;
 using DrinkManagerWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DrinkManagerWeb.Controllers
 {
@@ -28,7 +29,7 @@ namespace DrinkManagerWeb.Controllers
                 case "name_desc":
                     drinks = drinks.OrderByDescending(s => s.Name);
                     break;
-                default:    
+                default:
                     drinks = drinks.OrderBy(s => s.Name);
                     break;
             }
@@ -49,6 +50,58 @@ namespace DrinkManagerWeb.Controllers
             };
 
             return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormCollection data)
+        {
+
+            var ingredients = new List<Ingredient>();
+
+            foreach (var key in data.Keys)
+            {
+                if (key.Contains("Ingredient"))
+                {
+                    var ingredientData = data[key].ToString().Split(' ', 2);
+
+                    ingredients.Add(new Ingredient
+                    {
+                        Name = ingredientData[0],
+                        Amount = ingredientData[1]
+                    });
+                }
+            }
+            // image placeholder
+            var imageUrl = "https://medifactia.com/wp-content/uploads/2018/01/placeholder.png";
+
+            // if image data exists replace placeholder
+            if (data.ContainsKey("ImageUrl"))
+            {
+                imageUrl = data["ImageUrl"];
+            }
+
+
+            var newDrink = new Drink
+            {
+                Id = Guid.NewGuid().ToString(),
+                Ingredients = ingredients,
+                GlassType = data["GlassType"],
+                ImageUrl = imageUrl,
+                DrinkReview = null,
+                Category = data["Category"],
+                AlcoholicInfo = data["AlcoholicInfo"],
+                Instructions = data["Instructions"],
+                Name = data["Name"]
+            };
+
+            _db.Drinks.Add(newDrink);
+
+            return RedirectToAction(nameof(DrinkDetails), new { id = newDrink.Id });
         }
     }
 }
