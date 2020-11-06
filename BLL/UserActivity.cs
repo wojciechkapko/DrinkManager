b34b23c381772e0501ea459fed5a7281adfc2e36
Marek <wilkmarek0@gmail.com>
2020-11-06 19:28:00 +0100

    Added API project, created Activity Controller, created UserActivityDto and UserActivity classes

---
+++ BLL/UserActivity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BLL
+{
+    public class UserActivity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Action { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ ReportingModule/Controllers/ActivityController.cs
@@ -0,0 +1,44 @@
+﻿using BLL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ReportingModuleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        //POST: https://localhost:5001/api/Activity
+        [HttpPost]
+        public IActionResult CreateActivity(UserActivityDto model)
+        {
+            var activity = new UserActivity
+            {
+                Username = model.Username,
+                Action = model.Action,
+                Created = DateTime.Now
+            };
+            // Jakieś reposytorium co dodaje activity do bazy danych
+            // this._activityRep.Insert(activity);
+            if (string.IsNullOrEmpty(model.Username))
+            {
+                return BadRequest("Username was empty");
+            }
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetActivities()
+        {
+            var activities = new List<UserActivity> { new UserActivity { Username = "Tester" } };
+            return Ok(activities);
+        }
+    }
+
+    public class UserActivityDto
+    {
+        public string Username { get; set; }
+        public string Action { get; set; }
+    }
+
+}
