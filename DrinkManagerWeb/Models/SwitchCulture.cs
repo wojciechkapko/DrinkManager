2e1e50b67c62ae547a1eec706c111d3bbfa21d3c
Joanna Bukowska <joabuk2@gmail.com>
2020-11-24 23:12:27 +0100

    SwitchCulture added

---
+++ DrinkManagerWeb/Models/SwitchCulture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkManagerWeb.Models
+{
+    public class SwitchCulture
+    {
+        public CultureInfo CurrentUICulture { get; set; }
+        public List<CultureInfo> SupportedCultures { get; set; }
+    }
+}
---
+++ DrinkManagerWeb/Models/ViewModels/SwitchCultureComponents.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DrinkManagerWeb.Models.ViewModels
+{
+    public class SwitchCultureComponents : ViewComponent
+    {
+        private readonly IOptions<RequestLocalizationOptions> localizationOptions;
+
+        public SwitchCultureComponents(IOptions<RequestLocalizationOptions> localizationOptions)
+        {
+            this.localizationOptions = localizationOptions;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cultureFeature = HttpContext.Features.Get<IRequestCultureFeature>();
+            var model = new SwitchCulture
+            {
+                SupportedCultures = localizationOptions.Value.SupportedCultures.ToList(),
+                CurrentUICulture = cultureFeature.RequestCulture.UICulture
+            };
+
+            return View(model);
+        }
+    }
+}
