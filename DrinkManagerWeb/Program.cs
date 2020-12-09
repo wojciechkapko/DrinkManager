9b6b309f858052df9ff0bca6d99356ce33f0115b
Marcin Sieminski <marcin.sieminski@outlook.com>
2020-12-09 18:30:40 +0100

    Configure writting logs to database

--- DrinkManagerWeb/Program.cs
+++ DrinkManagerWeb/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 using System;
 
 namespace DrinkManagerWeb
@@ -10,17 +11,29 @@ namespace DrinkManagerWeb
     {
         public static void Main(string[] args)
         {
-            string logsPath = @"c:\XTMP\DrinkManagerLogs\log-.csv";
-
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
-                .WriteTo.File(logsPath, rollingInterval: RollingInterval.Minute)
+                .WriteTo.MSSqlServer("Server=(localdb)\\mssqllocaldb;Database=DrinksManager;Trusted_Connection=True;",
+                    new MSSqlServerSinkOptions {AutoCreateSqlTable = true, TableName = "AppLogs"})
                 .CreateLogger();
-            CreateHostBuilder(args).Build().Run();
+
+            try
+            {
+                Log.Information("DM: Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
