a68930b41aaafdeec6caec54836cf2730c3df321
Marcin Sieminski <marcin.sieminski@outlook.com>
2020-12-13 20:19:41 +0100

    Make password letters invisible (dots only) when creating or editing user

--- DrinkManagerWeb/Models/ViewModels/UserViewModel.cs
+++ DrinkManagerWeb/Models/ViewModels/UserViewModel.cs
@@ -13,9 +13,9 @@ namespace DrinkManagerWeb.Models.ViewModels
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [DataType(DataType.Password)]
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long and must contain at least one letter and one digit.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
