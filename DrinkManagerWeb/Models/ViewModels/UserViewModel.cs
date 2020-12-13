5c6637d519bedced44c1189e3c9e2937d56b0873
Marcin Sieminski <marcin.sieminski@outlook.com>
2020-12-13 21:02:00 +0100

    Bugfixes concerning entering password

--- DrinkManagerWeb/Models/ViewModels/UserViewModel.cs
+++ DrinkManagerWeb/Models/ViewModels/UserViewModel.cs
@@ -15,7 +15,7 @@ namespace DrinkManagerWeb.Models.ViewModels
 
         [DataType(DataType.Password)]
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long and must contain at least one letter and one digit.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long and must contain at least one letter.", MinimumLength = 6)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
