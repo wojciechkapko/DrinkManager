f13c6b60c6f7ea1e4eca2fad7bfcf8fe2aedabeb
Joanna Bukowska <joabuk2@gmail.com>
2020-12-14 21:57:03 +0100

    Added localization to login error messages

--- DrinkManagerWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ DrinkManagerWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -40,15 +41,17 @@ namespace DrinkManagerWeb.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "TheEmailFieldIsRequired")]
             [EmailAddress]
+            [DisplayName("Email")]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "ThePasswordFieldIsRequired")]
             [DataType(DataType.Password)]
+            [DisplayName("Password")]
+
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
 
