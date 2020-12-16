3447d6ba4f52daa6a72e81d5323b7a7073d8bf7b
Joanna Bukowska <joabuk2@gmail.com>
2020-12-16 21:45:38 +0100

    Email sender removed, corrected changing mail

--- DrinkManagerWeb/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ DrinkManagerWeb/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -1,14 +1,10 @@
 using BLL;
 using DrinkManagerWeb.Resources;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Localization;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace DrinkManagerWeb.Areas.Identity.Pages.Account.Manage
@@ -16,20 +12,14 @@ namespace DrinkManagerWeb.Areas.Identity.Pages.Account.Manage
     public class EmailModel : PageModel
     {
         private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly IEmailSender _emailSender;
         private readonly IStringLocalizer<SharedResource> _localizer;
 
 
         public EmailModel(
             UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager,
-            IEmailSender emailSender,
             IStringLocalizer<SharedResource> localizer)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
-            _emailSender = emailSender;
             _localizer = localizer;
 
         }
@@ -96,56 +86,14 @@ namespace DrinkManagerWeb.Areas.Identity.Pages.Account.Manage
             var email = await _userManager.GetEmailAsync(user);
             if (Input.NewEmail != email)
             {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                user.Email = Input.NewEmail;
+                user.UserName = Input.NewEmail;
+                await _userManager.UpdateAsync(user);
                 return RedirectToPage();
             }
 
             StatusMessage = _localizer["YourEmailIsUnchanged"];
 
-            return RedirectToPage();
-        }
-
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound(_localizer["UnableToLoadUserWithID"] + $" '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
-            var userId = await _userManager.GetUserIdAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { area = "Identity", userId = userId, code = code },
-                protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            StatusMessage = "Verification email sent. Please check your email.";
             return RedirectToPage();
         }
     }
