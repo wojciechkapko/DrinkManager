7f1d0700dee1305640e727db9c8697bb2f6da1f7
wkapko <wojciech.kapko@gmail.com>
2020-12-18 13:18:29 +0100

    fix for admin user protection

--- DrinkManagerWeb/Controllers/HomeController.cs
+++ DrinkManagerWeb/Controllers/HomeController.cs
@@ -10,7 +10,6 @@ namespace DrinkManagerWeb.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
