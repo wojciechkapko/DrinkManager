d3031e97fb6147595e7e6d21497e846195f7422e
wkapko <wojciech.kapko@gmail.com>
2020-12-26 12:01:10 +0100

    add agent file for basic requests to the api

---
+++ client-app/src/config/agent.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://localhost:5001/api";
+
+const responseBody = (response) => response.data;
+
+const requests = {
+  get: (url) => axios.get(url).then(responseBody),
+  post: (url, body) => axios.post(url, body).then(responseBody),
+  put: (url, body) => axios.put(url, body).then(responseBody),
+  delete: (url) => axios.delete(url).then(responseBody),
+};
+
+export default {
+  requests,
+};
