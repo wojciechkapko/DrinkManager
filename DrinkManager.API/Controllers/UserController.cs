b3cd07f674557b023a2e30bcbe2c3cfde6454aee
wkapko <wojciech.kapko@gmail.com>
2020-12-26 11:21:03 +0100

    Add action to get the current user to the User Controller

--- DrinkManager.API/Controllers/UserController.cs
+++ DrinkManager.API/Controllers/UserController.cs
@@ -1,6 +1,10 @@
 ﻿using BLL.Contracts.Requests;
 using BLL.Contracts.Responses;
 using BLL.Handlers;
+using BLL.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -10,13 +14,22 @@ namespace DrinkManager.API.Controllers
     {
         private readonly ILoginHandler _loginHandler;
         private readonly IRegisterHandler _registerHandler;
+        private readonly IUserAccessor _userAccessor;
+        private readonly IJwtGenerator _jwtGenerator;
+        private readonly UserManager<AppUser> _userManager;
 
         public UserController(
             ILoginHandler loginHandler,
-            IRegisterHandler registerHandler)
+            IRegisterHandler registerHandler,
+            IUserAccessor userAccessor,
+            IJwtGenerator jwtGenerator,
+            UserManager<AppUser> userManager)
         {
             _loginHandler = loginHandler;
             _registerHandler = registerHandler;
+            _userAccessor = userAccessor;
+            _jwtGenerator = jwtGenerator;
+            _userManager = userManager;
         }
 
         [HttpPost("login")]
@@ -45,6 +58,20 @@ namespace DrinkManager.API.Controllers
             }
 
             return Ok(user);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<LoginResponse>> CurrentUser()
+        {
+            var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+
+            return Ok(new LoginResponse
+            {
+                Username = user.UserName,
+                Token = _jwtGenerator.CreateToken(user),
+                Image = null
+            });
         }
     }
 }
