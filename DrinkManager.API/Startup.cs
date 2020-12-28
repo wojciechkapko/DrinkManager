2829c59dc59384af59b44261ae002ffa474a6ffe
wkapko <wojciech.kapko@gmail.com>
2020-12-28 12:18:26 +0100

    Prepare app for the realese

--- DrinkManager.API/Startup.cs
+++ DrinkManager.API/Startup.cs
@@ -39,10 +39,23 @@ namespace DrinkManager.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureDevelopmentServices(IServiceCollection services)
         {
             services.AddDbContext<DrinkAppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DrinkAppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            ConfigureServices(services);
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
             var builder = services.AddIdentityCore<AppUser>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
@@ -161,12 +174,13 @@ namespace DrinkManager.API
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             //app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
+
             app.UseRequestLocalization();
             app.UseRequestLocalizationCookies();
             app.UseSerilogRequestLogging();
@@ -180,6 +194,7 @@ namespace DrinkManager.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
